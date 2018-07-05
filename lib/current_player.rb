86efc6e432105a12a39ee4db826da7fa8df58fd5
Lucas Wang <lucas211w@gmail.com>
2018-07-05 21:26:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,11 +6,7 @@ def turn_count(board)
   return count
     end
   end
-  
+
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  return turn_count(board) % 2 ? "X" : "O"
 end
