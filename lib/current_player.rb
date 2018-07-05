3f7720e9a5085fe49d91f19696e215109024b3dd
Lucas Wang <lucas211w@gmail.com>
2018-07-05 21:23:51 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  count = 0
+  board.each do |square|
+    if square == "X" || square == "O"
+      count += 1
+  return count
+    end
+  end
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
